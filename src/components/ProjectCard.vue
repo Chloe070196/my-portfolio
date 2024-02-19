2d6ee1bcf79d1c692849e9586941b9fe552d7966
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-19 10:55:46 +0000

    feat: render project list

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import type { ProjectCardType } from "../types/index.ts"
+const props = defineProps<{
+    projectCard: ProjectCardType
+}>()
+const { title, description } = props.projectCard
+</script>
+
+<template>
+    <h3>{{ title }}</h3>
+    <p>{{ description }}</p>
+</template>
---
+++ src/components/ProjectList.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { ProjectCardType } from '@/types';
+import ProjectCard from './ProjectCard.vue';
+
+const props = defineProps<{
+    projects: ProjectCardType[]
+    }>()
+
+const { projects } = props
+</script>
+
+<template>
+    <section>
+        <ul>
+            <ProjectCard v-for="project in projects" v-bind:key="project.title" :project-card="project"></ProjectCard>
+        </ul>
+    </section>
+</template>
