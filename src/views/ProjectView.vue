c58c2619072bd8975d0bf0e28116c91181b01f02
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-19 10:55:25 +0000

    feat: add pages

---
+++ src/views/ProjectView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ProjectList from '@/components/ProjectList.vue';
+const projects = [{title: "project 1", description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem ex, neque exercitationem in nobis dolore porro ratione nostrum aliquam at magnam, vitae perspiciatis possimus? Aspernatur eos perspiciatis natus eius cum."}, {title: "project 1", description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem ex, neque exercitationem in nobis dolore porro ratione nostrum aliquam at magnam, vitae perspiciatis possimus? Aspernatur eos perspiciatis natus eius cum."}, {title: "project 1", description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem ex, neque exercitationem in nobis dolore porro ratione nostrum aliquam at magnam, vitae perspiciatis possimus? Aspernatur eos perspiciatis natus eius cum."}, {title: "project 1", description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem ex, neque exercitationem in nobis dolore porro ratione nostrum aliquam at magnam, vitae perspiciatis possimus? Aspernatur eos perspiciatis natus eius cum."}]
+</script>
+
+<template>
+    <h1>projects</h1>
+    <ProjectList :projects="projects"></ProjectList>
+</template>
