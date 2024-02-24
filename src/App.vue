3753f6c0ee3ac4788b4c58942bb7d44efc6af753
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-24 11:04:30 +0000

    feat: right aside

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import  RightAside from './components/RightAside.vue';
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/projects">Projects</RouterLink>
-      </nav>
+        <RightAside />
     </div>
   </header>
   <RouterView />
---
+++ src/components/RightAside.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import AboutSection from './AboutSection.vue';
+import NavBar from './NavBar.vue';
+</script>
+<template>
+    <aside>
+        <AboutSection />
+        <NavBar />
+    </aside>
+</template>
+
+<style scoped>
+    aside {
+        position: fixed;
+        right: 0;
+        top: 0;
+        margin: 20px;
+        width: 200px;
+    }
+</style>
