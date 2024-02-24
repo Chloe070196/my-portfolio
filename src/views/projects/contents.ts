cde684dbdbaf9da25b2efcfbfc03da03395b7136
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-24 11:06:36 +0000

    build: create content files

---
+++ src/views/projects/contents.ts
@@ -0,0 +1,44 @@
+const projects = [
+  {
+    title: 'project 1',
+    stack: ['JS', 'Taliwind', 'React'],
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem ex, neque exercitationem in nobis dolore porro ratione nostrum aliquam at magnam, vitae perspiciatis possimus? Aspernatur eos perspiciatis natus eius cum.',
+    gitHubUrl: '',
+    imageUrl: '',
+    imageDescription: '',
+
+  },
+  {
+    title: 'project 2',
+    stack: ['JS', 'Taliwind', 'React'],
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem ex, neque exercitationem in nobis dolore porro ratione nostrum aliquam at magnam, vitae perspiciatis possimus? Aspernatur eos perspiciatis natus eius cum.',
+    gitHubUrl: '',
+    imageUrl: '',
+    imageDescription: '',
+
+  },
+  {
+    title: 'project 3',
+    stack: ['JS', 'Taliwind', 'React'],
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem ex, neque exercitationem in nobis dolore porro ratione nostrum aliquam at magnam, vitae perspiciatis possimus? Aspernatur eos perspiciatis natus eius cum.',
+    gitHubUrl: '',
+    imageUrl: '',
+    imageDescription: '',
+
+  },
+  {
+    title: 'project 4',
+    stack: ['JS', 'Taliwind', 'React'],
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem ex, neque exercitationem in nobis dolore porro ratione nostrum aliquam at magnam, vitae perspiciatis possimus? Aspernatur eos perspiciatis natus eius cum.',
+    gitHubUrl: '',
+    imageUrl: '',
+    imageDescription: '',
+
+  }
+]
+
+export default projects
