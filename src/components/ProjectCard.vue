783303533df0bc4ee487bae7d14718283e57000b
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-24 11:07:44 +0000

    feat: update ProjectCard component

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -3,10 +3,17 @@ import type { ProjectCardType } from "../types/index.ts"
 const props = defineProps<{
     projectCard: ProjectCardType
 }>()
-const { title, description } = props.projectCard
+const { title, stack, description, gitHubLink, imageUrl, imageDescription } = props.projectCard
 </script>
 
 <template>
     <h3>{{ title }}</h3>
+    <ul class="inline-list">
+        <li v-for="item in stack" v-bind:key="item">
+            <h4>{{ item }}</h4>
+        </li>
+    </ul>
     <p>{{ description }}</p>
+    <img :href="imageUrl" :alt=" imageDescription"/>
+    <a :href="gitHubLink">GitHub Repository</a>
 </template>
