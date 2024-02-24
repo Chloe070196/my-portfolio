71a1c916e748ab501af201092c96bdb5f94de2ef
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-24 11:07:12 +0000

    build: project card type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,10 @@
 type ProjectCardType = {
     title: string,
+    stack: string[],
     description: string,
+    gitHubLink: string,
+    imageUrl: string,
+    imageDescription: string
 }
 
 export type { ProjectCardType }
