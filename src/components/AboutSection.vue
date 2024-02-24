6efa6528f27d439b0b29fd8bb387203c9a3bbca7
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-24 11:04:49 +0000

    feat: about section

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+    <section>
+        <h1>Chloé Zermatten</h1>
+        <h3>Full-stack software developer</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem similique quasi sint, sit repudiandae nemo ad deleniti molestiae quia, laborum ut corrupti eos totam quos aspernatur dolorem. Optio, laborum ipsum!</p>
+    </section>
+</template>
