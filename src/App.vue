fbe7fd7c125a8faf9f8623987ecb9ce689d5d0bb
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-29 15:15:39 +0000

    feat: import stylesheet

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import  RightAside from './components/RightAside.vue';
+import RightAside from './components/RightAside.vue';
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-        <RightAside />
-    </div>
-  </header>
-  <RouterView />
+  <RightAside />
+  <main>
+    <RouterView />
+  </main>
 </template>
+<style>
+    @import './style.css';
+</style>
