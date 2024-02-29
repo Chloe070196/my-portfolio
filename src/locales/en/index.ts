3088a1ac3f523767bc11bf40f7d0b5b9bb6952a9
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-29 19:19:19 +0000

    feat: french and english locales

---
+++ src/locales/en/index.ts
@@ -0,0 +1,13 @@
+const en = {
+  about: 'about',
+  softDev: 'Full-Stack Software Developer',
+  current: 'on-going',
+  deployed: 'deployed',
+  projects: 'projects',
+  shortAbout:
+    'I love a good brain-teaser, and enjoy collaborating with clients and co-workers to find effective and innovative solutions to real-life problems.',
+  based: 'Based in the UK',
+  proficiency: 'Fully proficient in English and French'
+}
+
+export { en }
---
+++ src/locales/fr/index.ts
@@ -0,0 +1,13 @@
+const fr = {
+  about: 'à propos',
+  softDev: 'Développeuse Full-Stack',
+  current: 'en cours',
+  deployed: 'déployé',
+  projects: 'Projets',
+  shortAbout:
+    "J'ai toujours aimé les casse-têtes et les challenges. Mais ce que je recherche surtout dans un emploi, c'est de pouvoir collaborer avec mes coollègues pour trouver et implémenter des solutions efficaces et innovantes aux problèmes du quotidien",
+  based: 'basée au Royaume Uni',
+  proficiency: 'bilingue français-anglais'
+}
+
+export { fr }
