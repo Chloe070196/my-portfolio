248b38e92ca50316a636dab5572db01bdf9434c9
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-29 15:15:17 +0000

    feat: add about contents

---
+++ src/views/about/contents.ts
@@ -0,0 +1,168 @@
+const timeLine = [
+  {
+    years: '2023-2024',
+    occupationList: [
+      {
+        title: 'Trainee Software Developer',
+        location: 'online',
+        organisation: 'Boolean UK',
+        description: '6-month long coding bootcamp',
+        qualification:'Boolean Certificate',
+        skills: [
+          {
+            name: 'problem-solving in JS',
+            descr: 'I completed multiple exercises by coding a solution in JS to a real-life inspired problem'
+          },
+          {
+            name: 'creating ERDs',
+            descr: 'I drafted relational databases structures and used mermaid to produce ERDs'
+          }, 
+          {
+            name: 'creating domain models',
+            descr: 'I identified and wrote user stories to then build domain models'
+          },
+          {
+            name: 'TDD',
+            descr: 'I wrote tests and then wrote code to the resulting specifications'
+          },
+          {
+            name: 'team-development',
+            descr: 'I took part to a 3-week simulation where we collaborated to develop a full-stack app in groups, following a methodology loosely based on SCRUM'
+          }, 
+          {
+            name: 'branch merges /conflicts resolution',
+            descr: 'As we moved on to more collaborative work, I had the opportunity to learn to resolve conflicts in both VS Code and GitHub'
+          }, 
+          {
+            name: 'Debugging',
+            descr: 'I have worked towards identifying the cause of a bug and writing a fix'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    years: '2022-2023',
+    occupationList: [
+      {
+        title: 'EFL Senior Teacher',
+        location: 'Brno, Czech Republic',
+        organisation: "ILC Brno",
+        description:
+          "I kept teaching, and trained newly-hired teacher and was responsible for updating and running the school's internal training programme. I also observed lessons, gave feedback, and provide mentoring sessions.",
+        qualification:'',
+        skills: [{ name: 'mentoring', descr: 'I supported less-experienced colleagues and provided guidance when required'}, { name: 'training professionals', descr: 'I facilitated the professional development of my coworkers by leading practical, peer-centered sessions'}]
+      }
+    ]
+  },
+  {
+    years: '2021-2022',
+    occupationList: [
+      {
+        title: 'EFL teacher',
+        location: 'Brno, Czech Republic',
+        organisation: 'ILC Brno',
+        description:
+          " I taught exam preparation courses (KET, PET, FCE, CAE) as well as a variety of general English classes to young learners and adults alike. I also did 1:1 and in-company teaching, preparing bespoken courses geared towards my students' needs.",
+        qualification:'',
+          skills: [
+          { name: 'needs analysis', descr: 'I carried out an in-depth analysis of the competency, goals and needs of my individual students' },
+          { name: 'goal setting', descr: 'I  collaborated with my students to establish measurable, discrete, and achievable goals' }
+        ]
+      }
+    ]
+  },
+  {
+    years: '2020-2021',
+    occupationList: [
+      {
+        title: 'EFL teacher',
+        location: 'Torun, Poland', 
+        organisation: 'IH Torun',
+        description:
+          'I took on a new challenge by adding exam preparation classes (FCE) for teenagers to the list of courses I taught, as well as by completing a certificate in advanced teaching methodology (). I also took part to an intensive 2-weeks young learners teaching course (IHYLCT) in order to hone my skills in this area of EFL',
+        qualification:'CAM (Cambridge Advanced Methodology Certificate) and IHCYLT (International House Certificate in Teaching Young Learners and Teenagers)',
+          skills: [{ name, descr: "" }]
+      },
+      {
+        title: 'Undergraduate Pschology Student',
+        location: 'online',
+        description: 'I worked on my final project - a comparative analysis of the perception a student might have of online therapy work contrasted organisation that of an experienced professional',
+        qualification:'BsC Psychology',
+        skills: [{ name, descr: "" }]
+      }
+    ]
+  },
+  {
+    years: '2019-2020',
+    occupationList: [
+      {
+        title: 'EFL teacher',
+        location: 'Torun, Poland',
+        organisation: 'IH Torun',
+        description:
+          'I started out by teaching mostly beginner kids classes , and focusing on developing classroom management strategies',
+        qualification:'',
+          skills: [{ name, descr: "" }]
+      },
+      {
+        title: 'Undergraduate Pschology Student',
+        location: 'online',
+        description: '',
+        qualification:'',
+        skills: [{ name: "stress management", descr: "working a full-time job in a field I was unfamiliar with whilst simultaneoulsy carrying on with my studies was challenging, and I developped various strategies to prevent and handle stress." }]
+      }
+    ]
+  },{
+    years: '2019 (april-may)',
+    occupationList: [
+      {
+        title: 'EFL trainee teacher',
+        location: 'Brussels',
+        organisation: 'LTTB (Language Teacher Training Belgium',
+        description: '',
+        qualification:'CELTA - pass A (Cambridge Certificate in Teaching English to Speakers of Other Languages',
+        skills: [{}]
+      }
+    ]
+  },
+  {
+    years: '2016-2019',
+    occupationList: [
+      { title: 'trainee contemporary dancer', location: 'Madrid, Bologna, Catania', organisation: 'Elephant in the Black Box, Art Factory International, MoDem Pro (Scenario Publicco)', 
+      description: '',
+      qualification:'',
+      skills: [{ name, descr: "" }] 
+      },
+      {
+        title: 'Undergraduate Pschology Student',
+        location: 'Switzerland, online',
+        organisation: 'Unidistance Suisse',
+        description: '',
+        qualification:'',
+        skills: [
+          { name: 'reading and understanding academic papers', descr:'' },
+          { name: 'reasearch and academic writing', descr:'' },
+          { name: 'self-guided learning', descr:'' },
+          { name: 'statistical analysis', descr:'' },
+          { name: 'differential diagnosis and case formulation (introduction)', descr:'' }
+        ]
+      }
+    ]
+  },
+  {
+    years: '2012-2015',
+    occupationList: [
+      {
+        title: 'Highschool',
+        location: 'Switerland (Nyon, Lausanne)',
+        organisation: 'Gymnase de Nyon, et au Gymnase du Bugnon',
+        description: '',
+        qualification:'Maturité Fédérale and Baccalauréat International (A-level equivalents)',
+        skills: [{}]
+      }
+    ]
+  }
+]
+
+export { timeLine }
