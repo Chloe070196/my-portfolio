2549108bbc3939275d32916cf1e384cf84bb26a6
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-29 15:11:12 +0000

    build: add timeline-related types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,28 @@
 type ProjectCardType = {
-    title: string,
-    stack: string[],
-    description: string,
-    gitHubLink: string,
-    imageUrl: string,
-    imageDescription: string
+  title: string
+  stack: string[]
+  description: string
+  gitHubLink: string
+  imageUrl: string
+  imageDescription: string
 }
 
-export type { ProjectCardType }
+type SkillType = {
+  name: string
+  descr: string
+}
+
+type OccupationType = {
+  title: string
+  location: string
+  organisation: string
+  description: string
+  qualification: string
+  skills: SkillType[]
+}
+
+type PeriodType = {
+  years: string
+  occupationList: OccupationType[]
+}
+export type { ProjectCardType, SkillType, PeriodType, OccupationType }
