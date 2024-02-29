93b8334510cf33c1dbca0b1ea8a7f877351d3d7b
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-29 15:16:42 +0000

    feat: stack component

---
+++ src/components/StackSection.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup></script>
+<template>
+    <section>
+        <h3>Stack</h3>
+        <p>HTML, CSS, JS</p>
+        <p>React, Vue.js, Express.js</p>
+        <p>Tailwind</p>
+        <p>VS Code, Git/ Git bash, Node.js</p>
+        <p>PostgreSQL</p>
+        <p>Github</p>
+        <p>conventional commits, DRY, SOLID, KISS </p>
+    </section>
+</template>
+
+<style>
+p {
+   padding-left: 32px; 
+}
+</style>
