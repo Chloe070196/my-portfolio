7072183df91f80088ac4255b890c78320d41dec5
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-29 19:18:41 +0000

    feat: implement vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
-import './assets/main.css'
-
+// create the app and implement its router
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
 app.use(router)
 
+// implement internationalization
+import { createI18n } from 'vue-i18n'
+import { fr } from './locales/fr/index' 
+import { en } from './locales/en/index' 
+
+const messages = { fr, en }
+const i18n = createI18n({
+    locale: 'fr',
+    fallbackLocale: 'fr',
+    messages,
+})
+app.use(i18n)
+console.log(i18n)
+
+// mount the app
 app.mount('#app')
