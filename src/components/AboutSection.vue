b0d4f62cdc3b4c36884a6d679ccc587ba2b2d65f
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-29 19:20:14 +0000

    feat: translate right aside

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,21 +1,21 @@
-<script lang="ts" setup></script>
-
+<script lang="ts" setup>
+</script>
 <template>
     <section>
         <h1>Chloé Zermatten</h1>
-        <h2>Full-stack software developer</h2>
-        <p>I love a good brain-teaser, and enjoy collaborating with clients and co-workers to find effective and innovative solutions to real-life problems.</p>
-        <p>Based in the UK</p>
-        <p>Fully proficient in English and French</p>
+        <h2>{{ this.$t('softDev') }}</h2>
+        <p>{{ this.$t('shortAbout') }}</p>
+        <p>{{ this.$t('based') }}</p>
+        <p>{{ this.$t('proficiency') }}</p>
     </section>
 </template>
 
 <style scoped>
-    h1 {
-        font-size: 32px;
-        line-height: 34px;
-        padding-bottom: 20px;
-        border-bottom: 1px solid rgba(255, 255, 255, 0.541);
-        margin-bottom: 5px;
-    }
+h1 {
+    font-size: 32px;
+    line-height: 34px;
+    padding: 0 0 20px 10px;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.541);
+    margin-bottom: 5px;
+}
 </style>
