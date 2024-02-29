2f34937685049037661c83dba2788dce53d015f6
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-29 15:21:47 +0000

    feat: add home placeholder

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -4,5 +4,13 @@
 
 <template>
   <main>
-  </main>
-</template>
+    <h2>Welcome!</h2>
+    <p> I'm Chloé, a former Senior EFL* Teacher with 4 years of professional experience working for private language
+      centers, and now embarquing on a new and exciting adventure as a Full-Stack Software Developer.</p>
+    <p> My life-long interest in how we learn and communicate as led me to become a BsC Psychology graduate, then a
+      language teacher, and now a Full-Stack Software Developer.</p>
+    <p>I like to build learning tools, and would love to work for a company that focuses either on education, or
+    communication, or both!</p>
+    <a> Get in touch! </a>
+    <p>*English as a Foreign Language</p>
+</main></template>
