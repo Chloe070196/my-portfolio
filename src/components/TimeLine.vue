5e9a9ae5cf1d6757eda3e7308d568f8ce6625cad
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-29 15:13:23 +0000

    feat: timeline

---
+++ src/components/TimeLine.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import TimeLineYear from './TimeLineYear.vue';
+import type { PeriodType } from '@/types';
+const props = defineProps<{
+    timeLine: PeriodType[]
+}>()
+const { timeLine }= props
+</script>
+
+<template>
+    <section class="scrollable-wrapper">
+        <div class="scrollable">
+            <TimeLineYear v-for="period in timeLine" v-bind:key="period.years" :period="period"/>
+        </div>
+    </section>
+</template>
