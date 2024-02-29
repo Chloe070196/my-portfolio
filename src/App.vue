abd2ce590205a8ee6e5339e22ed4d8b39faad6a6
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-29 19:21:04 +0000

    feat: website info section

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,17 @@ import RightAside from './components/RightAside.vue';
   <RightAside />
   <main>
     <RouterView />
+    <div class="website-info">
+        <p>Website created using Vue.js</p>
+        <p>©Chloé Zermatten, March 2024</p>
+      </div>
   </main>
 </template>
 <style>
     @import './style.css';
+    .website-info {
+        position: absolute;
+        bottom: 20px;
+        left: 0;
+    }
 </style>
