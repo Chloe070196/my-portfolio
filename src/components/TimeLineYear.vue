5e7b593a3a0d818bbc70da2644b6e85c8fae9477
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-29 15:13:03 +0000

    feat: timeline year

---
+++ src/components/TimeLineYear.vue
@@ -0,0 +1,26 @@
+
+<script lang="ts" setup>
+import TimeLineEvent from './TimeLineEvent.vue';
+import type { PeriodType } from '@/types';
+const props = defineProps<{
+    period: PeriodType
+}>()
+const { period } = props
+</script>
+
+<template>
+    <article>
+        <h2>{{ period.years }}</h2>
+        <section>
+            <TimeLineEvent v-for="occupation in period.occupationList" v-bind:key="occupation.title" :occupation="occupation" />
+        </section>
+    </article>
+</template>
+
+<style scoped>
+article {
+    display: grid;
+    grid-template-columns: 80px 1fr;
+    padding: 10px 20px;
+}
+</style>
