aa266bf0d560aca6b8b623dffa2315fb1f0966ee
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-29 15:12:17 +0000

    feat: timeline skill

---
+++ src/components/TimeLineSkill.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import type { SkillType } from '@/types';
+const props = defineProps<{
+    skill: SkillType
+}>()
+const { name, descr } = props.skill
+</script>
+
+<template>
+    <div>
+        <h5>{{ name }}</h5>
+        <p>{{ descr }}</p>
+    </div>
+</template>
