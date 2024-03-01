884f9de71f4921e6cbb47db7bd4faee21d400cab
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-01 17:07:13 +0000

    feat: new translated messages

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -1,13 +1,18 @@
 const en = {
-  about: 'about',
-  softDev: 'Full-Stack Software Developer',
+  about: 'About',
+  aboutEmployeurPreference: 'I like to build learning tools, and would love to work for a company that focuses either on education, or communication, or both!',
+  aboutExp: "I'm Chloé, a former Senior EFL* Teacher with 4 years of professional experience working for private language centers. Having sompleted a six-intensive Full Stack Development bootcamp with Boolean UK, I'm now embarquing on a new and exciting adventure as a Full-Stack Software Developer.",
+  aboutJobPreference: 'I love a good brain-teaser, and enjoy collaborating with clients and co-workers to find effective and innovative solutions to real-life problems.',
+  aboutMotivation: 'My life-long interest in how we learn and communicate as led me to become a BsC Psychology graduate, then a language teacher, and now a Full-Stack Software Developer.',
+  based: 'Based in the UK',
   current: 'on-going',
   deployed: 'deployed',
-  projects: 'projects',
-  shortAbout:
-    'I love a good brain-teaser, and enjoy collaborating with clients and co-workers to find effective and innovative solutions to real-life problems.',
-  based: 'Based in the UK',
-  proficiency: 'Fully proficient in English and French'
+  getIntouch: 'Get in touch',
+  proficiency: 'Fully proficient in English and French', 
+  projects: 'Projects',
+  softDev: 'Full-Stack Software Developer',
+  stack: 'Tech Stack',
+  welcome: 'Welcome!',
 }
 
 export { en }
--- src/locales/fr/index.ts
+++ src/locales/fr/index.ts
@@ -1,13 +1,19 @@
 const fr = {
-  about: 'à propos',
-  softDev: 'Développeuse Full-Stack',
-  current: 'en cours',
-  deployed: 'déployé',
-  projects: 'Projets',
-  shortAbout:
-    "J'ai toujours aimé les casse-têtes et les challenges. Mais ce que je recherche surtout dans un emploi, c'est de pouvoir collaborer avec mes coollègues pour trouver et implémenter des solutions efficaces et innovantes aux problèmes du quotidien",
-  based: 'basée au Royaume Uni',
-  proficiency: 'bilingue français-anglais'
+  about: "À Propos",
+  aboutEmployeurPreference: "J'aime construire des application qui permet ou facilitent les apprentissages, et cherche à travailler pour une entreprise qui soit investie dans le domaine de l'éducation, ou de la communication.",
+  aboutExp: "Avec 4 ans d'expérience en tant qu'enseignante de l'anglais language secondaire à l'étranger, puis 6 mois de formation intensive en Software Development avec Boolean UK, je me tourne maintenant vers le dévelopement d'application (Full Stack)",
+  aboutJobPreference:
+    "Ce qui me plaît dans le travail, c'est collaborer avec mes collègues pour trouver et implémenter des solutions efficaces et innovantes. Je suis toujours prête à me lancer dans un nouveau challenge.",
+  aboutMotivation: "Je me suis toujours intéressée à l'apprentissage et à la communication, ce qui m'a d'abord amenée à obtenir un BsC Psychology, puis à enseigner, et enfin à me former en tant que développeuse Full-Stack.",
+  based: "basée au Royaume Uni",
+  current: "en cours",
+  deployed: "déployé",
+  getIntouch: "Prendre contact",
+  proficiency: "bilingue français-anglais",
+  projects: "Projets",
+  softDev: "Développeuse Full-Stack",
+  stack: "Pile Technique (Stack)",
+  welcome: "Bienvenue!"
 }
 
 export { fr }
