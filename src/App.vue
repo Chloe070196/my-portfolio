df8e201d0960ef0e013c12241e52a7294d1992c0
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-02 12:12:45 +0000

    fix: re-render toggle locale button

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import RightAside from './components/RightAside.vue';
-import { inject } from 'vue'
-const i18n = inject('i18n')
-const toggleLocale = () => {
-  // @ts-ignore
-  if (i18n.global.locale === 'en') {
-    // @ts-ignore
-    i18n.global.locale = 'fr'
-    return
-  }
-  // @ts-ignore
-  if (i18n.global.locale === 'fr') {
-    // @ts-ignore
-    i18n.global.locale = 'en'
-  }
-}
-// @ts-ignore
-const locale = i18n.global.locale
+import ToggleLocale from './components/ToggleLocale.vue';
 </script>
 
 
@@ -29,8 +13,8 @@ const locale = i18n.global.locale
       <p>Website created using Vue.js</p>
       <p>©Chloé Zermatten, March 2024</p>
     </div>
+    <ToggleLocale />
   </main>
-  <button @click="toggleLocale">{{ locale }}</button>
 </template>
 <style>
 @import './style.css';
@@ -39,18 +23,5 @@ const locale = i18n.global.locale
   position: absolute;
   bottom: 20px;
   left: 0;
-}
-
-button {
-  position: fixed;
-  top: 20px;
-  left: 20px;
-  color: rgb(53, 24, 87);
-  width: 30px;
-  height: 26px;
-  font-weight: bold;
-  background-color: rgb(255, 255, 205);
-  border: 0;
-  z-index: 10;
 }
 </style>
---
+++ src/components/ToggleLocale.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { inject } from 'vue'
+import { ref } from 'vue'
+const i18n = inject('i18n')
+let alternativeLocaleOption = ref("Site en français")
+const toggleLocale = () => {
+    // @ts-ignore
+    if (i18n.global.locale === 'en') {
+        // @ts-ignore
+        i18n.global.locale = 'fr'
+        alternativeLocaleOption.value = 'Switch to english'
+        console.log(alternativeLocaleOption)
+        return
+    }
+    // @ts-ignore
+    if (i18n.global.locale === 'fr') {
+        // @ts-ignore
+        i18n.global.locale = 'en'
+        alternativeLocaleOption.value = 'Site en français'
+        console.log(alternativeLocaleOption)
+    }
+}
+</script>
+
+<template>
+    <button @click="toggleLocale">{{ alternativeLocaleOption }}</button>
+</template>
+
+<style scoped>
+button {
+    position: fixed;
+    top: 20px;
+    left: 20px;
+    color: rgb(53, 24, 87);
+    width: fit-content;
+    height: 26px;
+    font-weight: bold;
+    background-color: rgb(255, 255, 205);
+    border: 0;
+    z-index: 10;
+}
+</style> 
