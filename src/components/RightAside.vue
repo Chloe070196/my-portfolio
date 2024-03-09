c4fc4662bf260f316ad4892dfb677d8384cb8fb9
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-09 18:54:16 +0000

    feat: enhance visual separation between main and aside

--- src/components/RightAside.vue
+++ src/components/RightAside.vue
@@ -14,10 +14,13 @@ import StackSection from './StackSection.vue';
         position: fixed;
         right: 0;
         top: 0;
-        margin: 8px 20px;
+        margin: 0 20px;
         width: 360px;
         height: 100vh;
+        border-left: 2px solid rgba(255, 255, 255, 0.521);
+        background: linear-gradient(0.25turn,rgba(255, 255, 255, 0.15),rgba(255, 255, 255, 0.05), rgba(255, 255, 255, 0.02),rgba(255, 255, 255, 0.015),rgba(255, 255, 255, 0.01), rgba(255, 255, 255, 0));
     }
 </style>
 
 
+s
