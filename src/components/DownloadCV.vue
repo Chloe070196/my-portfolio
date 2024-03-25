290be78d4ded450410b549b8fae4d22da2699726
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-25 12:17:23 +0000

    fix: update cv link

--- src/components/DownloadCV.vue
+++ src/components/DownloadCV.vue
@@ -1,4 +1,4 @@
 <template>
-    <a className="smallLink" href="public\ChloeZermattenResume.pdf"
+    <a className="smallLink" href="https://github.com/Chloe070196/my-portfolio/blob/main/public/ChloeZermattenResume.pdf"
         download="CV-Chloe_Zermatten.pdf"><strong>download CV</strong></a>
 </template>
